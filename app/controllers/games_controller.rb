83f9ef668ebd12413be76a03462b520b3aa72312
Rachel Patterson <rachel.patterson.1986@gmail.com>
2015-02-17 17:38:00 -0500

    created/modified controllers for auth

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,8 @@
+Class GamesController < ApplicationsController
+  before_action :authenticate_user_from_token!
+
+  def show
+    @game = Game.find_by(params[:id])
+    render json: {:game => @game}, status: :ok
+  end
+end
