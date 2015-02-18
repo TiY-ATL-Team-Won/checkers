6ca941ef4180f010d75ebd75eafb86dd5286de6a
Matt Huhman <huhmanbeing@gmail.com>
2015-02-18 14:17:14 -0500

    forgot to rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150218135704) do
+ActiveRecord::Schema.define(version: 20150218184520) do
 
   create_table "games", force: :cascade do |t|
     t.text    "board"
@@ -41,6 +41,10 @@ ActiveRecord::Schema.define(version: 20150218135704) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "authentication_token"
+    t.integer  "wins"
+    t.integer  "losses"
+    t.integer  "draws"
+    t.integer  "experience"
   end
 
   add_index "users", ["authentication_token"], name: "index_users_on_authentication_token"
