38a003edd0492288e0c55a57b9358da46249d741
Rachel Patterson <rachel.patterson.1986@gmail.com>
2015-02-18 16:11:24 -0500

    i hate colons

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,7 +2,7 @@ class Game < ActiveRecord::Base
   has_many :players
   has_many :users, through: :players
   validates_length_of :users, maximum: 2
-  validates :users, uniqueness: {:scope :game_id}
+  validates :users, uniqueness: {scope: :game_id}
 
   serialize :board
 
