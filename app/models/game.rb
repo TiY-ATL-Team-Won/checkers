4c0348ef7389fc18271726fa307bc145c549f759
Matt Huhman <huhmanbeing@gmail.com>
2015-02-18 14:38:18 -0500

    Functioning game scaffold

--- app/models/game.rb
+++ app/models/game.rb
@@ -9,4 +9,30 @@ class Game < ActiveRecord::Base
   def as_json(opts={})
     super(:only => [:board, :turn_count])
   end
+
+  STARTING_BOARD = [[0, 1, 0, 1, 0, 1, 0, 1],
+                    [1, 0, 1, 0, 1, 0, 1, 0],
+ 					[0, 1, 0, 1, 0, 1, 0, 1],
+ 					[0, 0, 0, 0, 0, 0, 0, 0],
+ 					[0, 0, 0, 0, 0, 0, 0, 0],
+ 					[2, 0, 2, 0, 2, 0, 2, 0],
+ 					[0, 2, 0, 2, 0, 2, 0, 2],
+ 					[2, 0, 2, 0, 2, 0, 2, 0]]
+
+  def new_game!
+  	self.board = STARTING_BOARD
+  	self.turn_count = 1
+  	self.save
+  end
+
+  def self.waiting
+  	Game.where(:players_count => 1)
+  end
+
+  def self.active
+  	Game.where(:finished => false)
+  end
+  
+
+
 end
