265bafe88463a68e617c3a89363492b137a9669b
Ian Gawronski <iangawronski@gmail.com>
2015-02-19 15:32:02 -0500

    change to controller for auth

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -10,25 +10,25 @@ class GamesController < ApplicationController
     @game = Game.find(params[:id])
     @player1 = @game.users.first
     @player2 = @game.users.second
-    render json: {:game => @game, :users => {:player1 => @player1, :player2 => @player2}}, status: :ok
+    #render json: {:game => @game, :users => {:player1 => @player1, :player2 => @player2}}, status: :ok
+    render json: game_response(@game), status: :ok
   end
 
   def join
     @waiting = Game.waiting.first
     if @waiting
       @waiting.users << current_user
-      render json: {:users => @waiting.users, :game => @waiting}
+      render json: game_response(@waiting), status: :foo
     else
       @game = Game.create
       @game.users = [current_user]
       @game.new_game! #intial board and turn count stuff...
-      render json: {:users => @game.users, :game => @game}
+      render json: game_response(@game), status: :created
     end
   end
 
   private
     def game_response(game)
-      {:users => game.users, :game => game}
+      {:game => game.as_json(include: { users: { only: [:id, :email] }} )}
     end
-
 end
