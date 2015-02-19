66f643bf916f12c2b8c86f636514ab1f8f009f8a
Rachel Patterson <rachel.patterson.1986@gmail.com>
2015-02-18 22:56:47 -0500

    json ok?

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,21 +3,26 @@ class GamesController < ApplicationController
 
   def show
     @game = Game.find_by(params[:id])
-    @player1 = @game.user.first
-    @player2 = @game.user.second 
-    render json: {:game => @game}, status: :ok
+    @player1 = @game.users.first
+    @player2 = @game.users.second
+    # render json: {:game => @game, :player => {:user_id => @player1, :user_id => @player2}}, status: :ok
+    render json: {:game => @game, :player => @player1, :player => @player2}
+    #think abt what devs need. prob player 2 and 1
   end
 
   def join
     @waiting = Game.waiting.first
     if @waiting
       @waiting.users << current_user
-      redirect_to games_show_path(@waiting)
+      render json: {:authentication_token => @waiting.users, :game => @waiting}
+    #  redirect_to games_show_path(@waiting)
     else
       @game = Game.create
       @game.users = [current_user]
-      @game.new_game! #intial board and turn count stuff...
-      redirect_to games_show_path(@game)
+      @game.new_game! #intial board and turn count stuff... thats what front end is rendering?
+    #  redirect_to games_show_path(@game)
+      render json: {:authentication_token => @game.users, :game => @game}
     end
   end
+
 end
