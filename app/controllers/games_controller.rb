f62cc26aa1e89ab5fdcfac9962121cd658690f2c
Rachel Patterson <rachel.patterson.1986@gmail.com>
2015-02-19 14:45:17 -0500

    removed comments

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -5,26 +5,24 @@ class GamesController < ApplicationController
     @game = Game.find_by(params[:id])
     @player1 = @game.users.first
     @player2 = @game.users.second
-    #render json: {:game => @game, :player => {:user_id => @player1, :user_id => @player2}}, status: :ok
     render json: {:game => @game, :users => {:player1 => @player1, :player2 => @player2}}, status: :ok
-    # render json: {:game => @game, :player => @player1, :player => @player2}
-    #render json: {:game => @game.users}
-    #think abt what devs need. prob player 2 and 1
   end
 
   def join
     @waiting = Game.waiting.first
     if @waiting
       @waiting.users << current_user
-      render json: {:authentication_token => @waiting.users, :game => @waiting}
-    #  redirect_to games_show_path(@waiting)
+      render json: {:users => @waiting.users, :game => @waiting}
     else
       @game = Game.create
       @game.users = [current_user]
-      @game.new_game! #intial board and turn count stuff... thats what front end is rendering?
-    #  redirect_to games_show_path(@game)
-      render json: {:authentication_token => @game.users, :game => @game}
+      @game.new_game!
+      render json: {:users => @game.users, :game => @game}
     end
   end
 
+  private
+  def game_response(game)
+    {:users => game.users, :game => game}
+  end
 end
