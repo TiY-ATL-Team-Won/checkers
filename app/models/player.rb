56ca700bdf0126d36fbd415ede046d9b9169fc79
Ian Gawronski <iangawronski@gmail.com>
2015-02-22 10:05:52 -0500

    fixing bug

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,6 +1,6 @@
 class Player < ActiveRecord::Base
 	belongs_to :user
-	belongs_to :game, :counter_cache => true
+	belongs_to :game #:counter_cache => true
 
 	validates_uniqueness_of :user_id, scope: :game_id
 
