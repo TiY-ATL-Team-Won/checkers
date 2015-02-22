4f1cc4a8e51bea9081f8d184860d1cc474a0916d
Matt Huhman <huhmanbeing@gmail.com>
2015-02-22 13:35:47 -0500

    Finished king logic

--- app/models/game.rb
+++ app/models/game.rb
@@ -25,11 +25,9 @@ class Game < ActiveRecord::Base
   def new_game!
   	self.board = STARTING_BOARD
     # self.update_attribute :board, STARTING_BOARD
-    #binding.pry
     #self.users.first
     #self.players_count = 1
   	self.turn_count = 1
-  	#binding.pry
 
   	self.save
   end
@@ -59,22 +57,18 @@ class Game < ActiveRecord::Base
   # this renders having a black and red_move function unnecessary
 
   def test_move(params)
-    #binding.pry
     @game = Game.find(params["id"].to_i)
     user_id = params["user_id"]
     start_x = params["x"]
     start_y = params["y"]
     current_user = User.find(user_id)
     type = @game.board[start_y][start_x]
-    #binding.pry
 
   	if ((type == 1 || type == 3) && current_user = @game.players.first) || ((type == 2 || type == 4) && current_user = @game.players.second)
   	  case type
   	  when 1
-        #binding.pry
   	    test = test_black_move(start_x, start_y, type, params["move"])
   	  when 2
-        #binding.pry
   		  test = test_red_move(start_x, start_y, type, params["move"])
   	  when 3
   		  test = test_king_move(start_x, start_y, type, params["move"])
@@ -135,7 +129,21 @@ class Game < ActiveRecord::Base
       if legal_king_move(st_x, st_y, mv_x, mv_y, @game.board)
         @game.board[st_y][st_x] = 0
         @game.board[mv_y][mv_x] = type
-        
+        if type == 3
+          @game.turn_count = 2
+        elsif type == 4
+          @game.turn_count = 1
+        end
+      else
+        return {:board => @game.board, :message_type => 0, :message => "Move unsuccessful"}
+      end
+    end
+    @game.save
+    {:board => @game.board, :turn_count => @game.turn_count, :message_type => 1, :message => "Move successful"}
+  end
+
+
+
 
 
 
